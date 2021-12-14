25b20f0959a0c9d7b10636ca004bdb987dd2d587
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 20:07:23 +0200

    QR code generator

---
+++ src/main/java/com/odeyalo/music/analog/spotify/utils/QrCodeGenerator.java
@@ -0,0 +1,24 @@
+package com.odeyalo.music.analog.spotify.utils;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class QrCodeGenerator {
+    public static void createQRCode(String qrCodeData, String filePath,
+                                    String charset, Map hintMap,
+                                    int qrCodeheight, int qrCodewidth)
+            throws WriterException, IOException {
+        BitMatrix matrix = new MultiFormatWriter().encode(
+                new String(qrCodeData.getBytes(charset), charset),
+                BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
+        MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath
+                .lastIndexOf('.') + 1), new File(filePath));
+    }
+}
