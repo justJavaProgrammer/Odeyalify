3954a221a40d35418164809a3af2bbf75a4133ca
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 20:09:30 +0200

    Register service. All register services must implement this interface

---
+++ src/main/java/com/odeyalo/music/analog/spotify/services/register/RegisterService.java
@@ -0,0 +1,13 @@
+package com.odeyalo.music.analog.spotify.services.register;
+
+import com.odeyalo.music.analog.spotify.dto.JWTResponseDto;
+import com.odeyalo.music.analog.spotify.entity.User;
+import com.odeyalo.music.analog.spotify.exceptions.UserException;
+
+public interface RegisterService {
+
+    JWTResponseDto register(User user) throws UserException;
+
+    boolean isUserAlreadyExist(User user);
+
+}
