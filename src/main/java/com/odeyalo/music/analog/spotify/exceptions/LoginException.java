b694f91b04d6188b4a3fd3dfc15d75d0239d8736
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:59:57 +0200

    This exception throws out when error was occurred during login process

---
+++ src/main/java/com/odeyalo/music/analog/spotify/exceptions/LoginException.java
@@ -0,0 +1,11 @@
+package com.odeyalo.music.analog.spotify.exceptions;
+
+public class LoginException extends RuntimeException {
+    public LoginException(String message) {
+        super(message);
+    }
+
+    public LoginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
