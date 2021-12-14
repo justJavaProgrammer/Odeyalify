fae07357d9d3e01851fa6aba5d7bd4ec278a5d10
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:36:16 +0200

    Constants

---
+++ src/main/java/com/odeyalo/music/analog/spotify/AudioConstants.java
@@ -0,0 +1,7 @@
+package com.odeyalo.music.analog.spotify;
+
+public class AudioConstants {
+    public static final String DEFAULT_PATH_TO_SAVE_AUDIO_DATA =
+            "C:\\Users\\thepr_2iz2cnv\\IdeaProjects\\spotify\\src\\main\\resources\\music\\";
+
+}
