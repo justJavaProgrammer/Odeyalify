c33b32ac6ab0ad032ea13cc5cd182dee5cd74098
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:49:32 +0200

    Exception when error was occurred during uploading file

---
+++ src/main/java/com/odeyalo/music/analog/spotify/exceptions/FileUploadException.java
@@ -0,0 +1,11 @@
+package com.odeyalo.music.analog.spotify.exceptions;
+
+public class FileUploadException extends RuntimeException {
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
