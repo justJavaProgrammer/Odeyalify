10bc2a98bee0fcbb7c181c83d49258383db267e8
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 20:04:30 +0200

    Information about playlist

---
+++ src/main/java/com/odeyalo/music/analog/spotify/services/info/dao/PlaylistInformation.java
@@ -0,0 +1,18 @@
+package com.odeyalo.music.analog.spotify.services.info.dao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.odeyalo.music.analog.spotify.dto.PlaylistDetailDTO;
+
+public class PlaylistInformation extends Information<PlaylistDetailDTO> {
+    private PlaylistDetailDTO playlistDetailDTO;
+
+    public PlaylistInformation(PlaylistDetailDTO playlistDetailDTO) {
+        this.playlistDetailDTO = playlistDetailDTO;
+    }
+
+    @JsonProperty("playlistDTO")
+    @Override
+    public PlaylistDetailDTO getInformation() {
+        return this.playlistDetailDTO;
+    }
+}
