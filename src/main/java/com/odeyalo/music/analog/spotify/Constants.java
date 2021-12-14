5699a82b1353bc642d51b1cf6566a0cab1e299c2
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:42:55 +0200

    Constants

---
+++ src/main/java/com/odeyalo/music/analog/spotify/Constants.java
@@ -0,0 +1,10 @@
+package com.odeyalo.music.analog.spotify;
+
+import com.odeyalo.music.analog.spotify.entity.enums.Role;
+
+public class Constants {
+    public static final String USER_NOT_FOUND_DEFAULT_MESSAGE =
+            "User with email %s not found.";
+
+    public static final Role DEFAULT_ROLE = Role.USER;
+}
