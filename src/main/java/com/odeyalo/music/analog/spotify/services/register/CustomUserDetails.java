50883c852c8ba4f3497afb638b0fbc80d36fca05
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:45:17 +0200

    Implementation of UserDetails

---
+++ src/main/java/com/odeyalo/music/analog/spotify/services/register/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package com.odeyalo.music.analog.spotify.services.register;
+
+import com.odeyalo.music.analog.spotify.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.user.getRoles();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+}
