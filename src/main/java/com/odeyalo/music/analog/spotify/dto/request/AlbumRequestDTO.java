787581c8cac2e7ecdd98bda03b1f4a67175e2225
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:16:14 +0200

    Contains album id for requests

---
+++ src/main/java/com/odeyalo/music/analog/spotify/dto/request/AlbumRequestDTO.java
@@ -0,0 +1,14 @@
+package com.odeyalo.music.analog.spotify.dto.request;
+
+public class AlbumRequestDTO {
+    private String albumId;
+
+
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+}
