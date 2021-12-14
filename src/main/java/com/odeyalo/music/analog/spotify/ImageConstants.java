cc3e39a83696169603d77684503843a100e37a4b
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:51:47 +0200

    Constants

---
+++ src/main/java/com/odeyalo/music/analog/spotify/ImageConstants.java
@@ -0,0 +1,11 @@
+package com.odeyalo.music.analog.spotify;
+
+public class ImageConstants {
+    public static final String DEFAULT_IMAGE_FILE_URL = "http://localhost:63342/spotify/img/avatars/";
+    public static final String DEFAULT_ALBUM_IMAGE_COVER_URL = DEFAULT_IMAGE_FILE_URL + "defaultAvatars/default_album_image_cover.jpg";
+    public static final String ALBUM_IMAGES_URL = DEFAULT_IMAGE_FILE_URL + "albumCoverImages/";
+    public static final String DEFAULT_USER_AVATAR_URL =  DEFAULT_IMAGE_FILE_URL + "defaultAvatars/default_avatar.png";
+
+    public static final String ROOT_PATH_TO_IMAGES = "C:\\Users\\thepr_2iz2cnv\\IdeaProjects\\spotify\\src\\main\\resources\\img\\avatars\\";
+    public static final String ROOT_PATH_TO_ALBUM_COVER_IMAGE = ROOT_PATH_TO_IMAGES + "albumCoverImages\\";
+}
