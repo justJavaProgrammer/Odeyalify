a0fd59445ac8abf628c11058ab458c01a26d8583
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-14 19:11:02 +0200

    Factory for create or transform playlist entity

---
+++ src/main/java/com/odeyalo/music/analog/spotify/factory/AlbumFactory.java
@@ -0,0 +1,36 @@
+package com.odeyalo.music.analog.spotify.factory;
+
+import com.odeyalo.music.analog.spotify.dto.AlbumDetailDTO;
+import com.odeyalo.music.analog.spotify.dto.request.AlbumRequestDTO;
+import com.odeyalo.music.analog.spotify.entity.Album;
+import com.odeyalo.music.analog.spotify.repositories.AlbumRepository;
+import com.odeyalo.music.analog.spotify.services.info.dao.AlbumInformation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlbumFactory {
+    private static  AlbumRepository albumRepository;
+
+    @Autowired
+    public AlbumFactory(AlbumRepository albumRepository) {
+        AlbumFactory.albumRepository = albumRepository;
+    }
+
+    public static Album buildAlbumFromAlbumDTO(AlbumRequestDTO dto) {
+        String albumId = dto.getAlbumId();
+        return AlbumFactory.albumRepository.findById(albumId).orElse(null);
+    }
+    public static Album buildAlbumFromAlbumDetailDTO(AlbumDetailDTO dto) {
+        String albumId = dto.getAlbumId();
+        return AlbumFactory.albumRepository.findById(albumId).orElse(null);
+    }
+
+    public static AlbumDetailDTO buildAlbumDetailDTOFromAlbumEntity(Album album) {
+        return AlbumDetailDTO.buildAlbumDtoFromAlbumEntity(album);
+    }
+    public static Album buildAlbumFromAlbumInformation(AlbumInformation information) {
+        String albumId = information.getObject().getAlbumId();
+        return AlbumFactory.albumRepository.findById(albumId).orElse(null);
+    }
+}
