2341ffe3edfe4dbb8950bdb5702f12222208e5c5
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-19 14:08:21 +0200

    Upload file controller

--- src/main/java/com/odeyalo/music/analog/spotify/controllers/UploadFileController.java
+++ src/main/java/com/odeyalo/music/analog/spotify/controllers/UploadFileController.java
@@ -2,8 +2,12 @@ package com.odeyalo.music.analog.spotify.controllers;
 
 import com.odeyalo.music.analog.spotify.dto.request.AlbumWithImageDTO;
 import com.odeyalo.music.analog.spotify.entity.Album;
-import com.odeyalo.music.analog.spotify.services.AlbumEntitySaver;
+import com.odeyalo.music.analog.spotify.entity.song.Song;
+import com.odeyalo.music.analog.spotify.exceptions.NotSupportedFileTypeException;
+import com.odeyalo.music.analog.spotify.services.saver.AlbumEntitySaver;
+import com.odeyalo.music.analog.spotify.services.saver.SongEntitySaver;
 import com.odeyalo.music.analog.spotify.services.install.AlbumAvatarInstallerService;
+import com.odeyalo.music.analog.spotify.utils.UserDetailsUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,21 +17,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.*;
 
 @RestController
 @RequestMapping("/upload/")
 @CrossOrigin(origins = "*")
 public class UploadFileController {
-    private Logger logger = LoggerFactory.getLogger(UploadFileController.class);
     private AlbumEntitySaver albumEntitySaver;
-    private AlbumAvatarInstallerService avatarInstallerService;
-
-    public UploadFileController(AlbumEntitySaver albumEntitySaver, AlbumAvatarInstallerService avatarInstallerService) {
+    public UploadFileController(AlbumEntitySaver albumEntitySaver, SongEntitySaver saver) {
         this.albumEntitySaver = albumEntitySaver;
-        this.avatarInstallerService = avatarInstallerService;
     }
-    //todo make a code review and rewrite it with better practice
     @PostMapping("/album")
     public ResponseEntity<?> uploadAlbum(
                                          @RequestPart Album album,
@@ -36,12 +36,10 @@ public class UploadFileController {
                                          Authentication authentication) throws Exception {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         AlbumWithImageDTO albumWithImageDTO = new AlbumWithImageDTO(album, albumCover);
-        this.albumEntitySaver.save(songs, albumWithImageDTO, userDetails);
-        this.logger.info("album: {}", album);
+        this.albumEntitySaver.save(songs, albumWithImageDTO, UserDetailsUtils.getUserFromUserDetails(userDetails));
         Map<String, Object> body = new HashMap<>();
         body.put("success", true);
         body.put("message", "Success upload album to the server");
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
-
 }
