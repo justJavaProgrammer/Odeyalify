eb09eb60de4033bb84a66c68ef75e632a49bf33b
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-19 13:57:49 +0200

    Save album entity in DB

---
+++ src/main/java/com/odeyalo/music/analog/spotify/services/saver/AlbumEntitySaver.java
@@ -0,0 +1,80 @@
+package com.odeyalo.music.analog.spotify.services.saver;
+
+import com.odeyalo.music.analog.spotify.answer.UploadFileAnswer;
+import com.odeyalo.music.analog.spotify.dto.request.AlbumWithImageDTO;
+import com.odeyalo.music.analog.spotify.entity.Album;
+import com.odeyalo.music.analog.spotify.entity.Artist;
+import com.odeyalo.music.analog.spotify.entity.User;
+import com.odeyalo.music.analog.spotify.entity.song.Song;
+import com.odeyalo.music.analog.spotify.exceptions.NotSupportedFileTypeException;
+import com.odeyalo.music.analog.spotify.factory.ArtistFactory;
+import com.odeyalo.music.analog.spotify.services.install.AlbumAvatarInstallerService;
+import com.odeyalo.music.analog.spotify.utils.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class AlbumEntitySaver implements Saver<AlbumWithImageDTO> {
+    private final AlbumAvatarInstallerService avatarInstallerService;
+    private final SongEntitySaver songEntitySaver;
+
+    @Autowired
+    public AlbumEntitySaver(AlbumAvatarInstallerService avatarInstallerService, SongEntitySaver songEntitySaver) {
+        this.avatarInstallerService = avatarInstallerService;
+        this.songEntitySaver = songEntitySaver;
+    }
+
+    @Override
+    @Transactional
+    public void save(MultipartFile[] songFiles, AlbumWithImageDTO albumWithImageDTO, User user) throws Exception {
+        if (songFiles.length != albumWithImageDTO.getAlbum().getSongCount())
+            throw new NoSuchElementException("Audio files more then album info");
+        UploadFileAnswer answer = checkFiles(songFiles);
+        if (!answer.isSuccess())
+            throw new NotSupportedFileTypeException(answer.getCause());
+        doSave(songFiles, albumWithImageDTO, user);
+    }
+
+    private void doSave(MultipartFile[] songFiles, AlbumWithImageDTO albumWithImageDTO, User user) throws Exception {
+        Album album = albumWithImageDTO.getAlbum();
+        Artist artist = ArtistFactory.getArtistFromUser(user);
+        Album savedAlbum = this.saveAlbumWithAvatar(this.buildAlbum(album, artist), albumWithImageDTO.getAlbumCover());
+        List<Song> songs = this.setAlbumForSongs(new ArrayList<>(album.getSongs()), savedAlbum);
+        this.songEntitySaver.save(songFiles, songs, user);
+    }
+
+    private UploadFileAnswer checkFiles(MultipartFile[] files) {
+        for (MultipartFile file : files) {
+            if (!FileUtils.isAudioContentFile(file)) {
+                return new UploadFileAnswer(false, String.format("File with type: %s not supported. Supported file types: mp3, ogg", FileUtils.getFileExtension(file)));
+            }
+        }
+        return new UploadFileAnswer(true);
+    }
+
+    private Album buildAlbum(Album album, Artist artist) {
+        artist.addAlbum(album);
+        album.setYearIssue(LocalDate.now().getYear());
+        album.setArtist(artist);
+        album.setSongCount(album.getSongs().size());
+        return album;
+    }
+
+    private Album saveAlbumWithAvatar(Album album, MultipartFile file) throws Exception {
+        return this.avatarInstallerService.installAvatar(file, album);
+    }
+
+    private List<Song> setAlbumForSongs(List<Song> songs, Album album) {
+        songs.forEach(song -> {
+            song.setAlbum(album);
+        });
+        return songs;
+    }
+}
