2008a76167563a67cdab9a231f4dbbe7ab18b078
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-19 14:03:40 +0200

    Create and save playlist entity in DB

--- src/main/java/com/odeyalo/music/analog/spotify/services/handle/create/PlaylistCreateHandler.java
+++ src/main/java/com/odeyalo/music/analog/spotify/services/handle/create/PlaylistCreateHandler.java
@@ -1,4 +1,4 @@
-package com.odeyalo.music.analog.spotify.services.handle;
+package com.odeyalo.music.analog.spotify.services.handle.create;
 
 import com.odeyalo.music.analog.spotify.entity.Playlist;
 import com.odeyalo.music.analog.spotify.entity.User;
@@ -20,7 +20,6 @@ public class PlaylistCreateHandler implements CreateHandler<Playlist> {
     @Override
     public void create(Playlist playlist, User user) {
         Playlist createdPlaylist = createPlaylist(playlist);
-//        playlist.addUser(user);
         Playlist save = this.playlistRepository.save(createdPlaylist);
         this.logger.info("Save playlist with id: {}", save.getId());
 
@@ -28,12 +27,12 @@ public class PlaylistCreateHandler implements CreateHandler<Playlist> {
 
     private Playlist createPlaylist(Playlist playlist) {
         playlist.setAuditions(new AtomicLong(0));
-        playlist.setLink(generateLink(playlist.getId()));
+        playlist.setLink(generateLink());
         this.logger.info("Generate playlist with data: {}", playlist);
         return playlist;
     }
 
-    private String generateLink(String playlistId) {
+    private String generateLink() {
         return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 10);
     }
 }
