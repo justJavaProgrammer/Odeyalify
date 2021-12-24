7fc459c06428e83446f38003337b83122bdc5c1b
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-24 20:55:48 +0200

    Minor changes

--- src/main/java/com/SpotifyApplication.java
+++ src/main/java/com/SpotifyApplication.java
@@ -3,8 +3,10 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @ComponentScan("com.odeyalo.music.analog")
 public class SpotifyApplication {
     public static void main(String[] args) {
--- src/test/java/com/odeyalo/music/analog/spotify/SpotifyApplicationTests.java
+++ src/test/java/com/odeyalo/music/analog/spotify/SpotifyApplicationTests.java
@@ -2,8 +2,10 @@ package com.odeyalo.music.analog.spotify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan(lazyInit = true)
 class SpotifyApplicationTests {
 
     @Test
