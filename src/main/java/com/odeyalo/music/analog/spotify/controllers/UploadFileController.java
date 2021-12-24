53cf3e947ef5dded4115695d578efdefd92f0a64
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-24 20:56:55 +0200

    Feature for future

--- src/main/java/com/odeyalo/music/analog/spotify/controllers/UploadFileController.java
+++ src/main/java/com/odeyalo/music/analog/spotify/controllers/UploadFileController.java
@@ -1,15 +1,12 @@
 package com.odeyalo.music.analog.spotify.controllers;
 
+import com.odeyalo.music.analog.spotify.converter.AudioConverter;
 import com.odeyalo.music.analog.spotify.dto.request.AlbumWithImageDTO;
+import com.odeyalo.music.analog.spotify.dto.request.YoutubeToMp3ConvertRequestDTO;
 import com.odeyalo.music.analog.spotify.entity.Album;
-import com.odeyalo.music.analog.spotify.entity.song.Song;
-import com.odeyalo.music.analog.spotify.exceptions.NotSupportedFileTypeException;
 import com.odeyalo.music.analog.spotify.services.saver.AlbumEntitySaver;
 import com.odeyalo.music.analog.spotify.services.saver.SongEntitySaver;
-import com.odeyalo.music.analog.spotify.services.install.AlbumAvatarInstallerService;
 import com.odeyalo.music.analog.spotify.utils.UserDetailsUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,23 +14,27 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/upload/")
 @CrossOrigin(origins = "*")
 public class UploadFileController {
-    private AlbumEntitySaver albumEntitySaver;
-    public UploadFileController(AlbumEntitySaver albumEntitySaver, SongEntitySaver saver) {
+    private final AlbumEntitySaver albumEntitySaver;
+    private final AudioConverter audioConverter;
+
+    public UploadFileController(AlbumEntitySaver albumEntitySaver, AudioConverter audioConverter) {
         this.albumEntitySaver = albumEntitySaver;
+        this.audioConverter = audioConverter;
     }
+
     @PostMapping("/album")
     public ResponseEntity<?> uploadAlbum(
-                                         @RequestPart Album album,
-                                         @RequestPart MultipartFile[] songs,
-                                         @RequestPart MultipartFile albumCover,
-                                         Authentication authentication) throws Exception {
+            @RequestPart Album album,
+            @RequestPart MultipartFile[] songs,
+            @RequestPart MultipartFile albumCover,
+            Authentication authentication) throws Exception {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         AlbumWithImageDTO albumWithImageDTO = new AlbumWithImageDTO(album, albumCover);
         this.albumEntitySaver.save(songs, albumWithImageDTO, UserDetailsUtils.getUserFromUserDetails(userDetails));
@@ -41,5 +42,11 @@ public class UploadFileController {
         body.put("success", true);
         body.put("message", "Success upload album to the server");
         return new ResponseEntity<>(body, HttpStatus.OK);
+    }
+
+    @PostMapping("/youtube")
+    public ResponseEntity<?> uploadSongFromYoutube(@RequestBody YoutubeToMp3ConvertRequestDTO dto) {
+        String path = this.audioConverter.convert(dto.getUrl());
+        return new ResponseEntity<>(path, HttpStatus.OK);
     }
 }
