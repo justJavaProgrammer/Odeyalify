a9c45070f80823f8386dce506ebb252d28772fa3
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-24 18:46:41 +0200

    Added file storage

--- src/main/java/com/odeyalo/music/analog/spotify/services/install/AlbumAvatarInstallerService.java
+++ src/main/java/com/odeyalo/music/analog/spotify/services/install/AlbumAvatarInstallerService.java
@@ -3,6 +3,7 @@ package com.odeyalo.music.analog.spotify.services.install;
 import com.odeyalo.music.analog.spotify.constants.ImageConstants;
 import com.odeyalo.music.analog.spotify.entity.Album;
 import com.odeyalo.music.analog.spotify.services.upload.UploadFileService;
+import com.odeyalo.music.analog.spotify.storage.FileStorage;
 import com.odeyalo.music.analog.spotify.update.AlbumEntityUpdater;
 import com.odeyalo.music.analog.spotify.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,10 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 public class AlbumAvatarInstallerService implements AvatarInstaller<Album> {
     private UploadFileService uploadFileService;
     private AlbumEntityUpdater aLbumEntityUpdater;
-
-    public AlbumAvatarInstallerService(@Qualifier("uploadImageFileService") UploadFileService uploadFileService, AlbumEntityUpdater aLbumEntityUpdater) {
+    private FileStorage fileStorage;
+    public AlbumAvatarInstallerService(@Qualifier("uploadImageFileService") UploadFileService uploadFileService, AlbumEntityUpdater aLbumEntityUpdater, @Qualifier("localImageFileStorage") FileStorage fileStorage) {
         this.uploadFileService = uploadFileService;
         this.aLbumEntityUpdater = aLbumEntityUpdater;
+        this.fileStorage = fileStorage;
     }
 
     @Override
@@ -25,8 +27,8 @@ public class AlbumAvatarInstallerService implements AvatarInstaller<Album> {
             album.setCoverImageUrl(ImageConstants.DEFAULT_ALBUM_IMAGE_COVER_URL);
             return this.aLbumEntityUpdater.update(album);
         }
-        String fileName = this.uploadFileService.upload(file, album.getArtist().getUser());
-        album.setCoverImageUrl(ImageConstants.DEFAULT_IMAGE_FILE_URL + fileName + FileUtils.getFileExtension(file));
+        String fileName = this.uploadFileService.upload(file, album.getArtist().getUser(), fileStorage);
+        album.setCoverImageUrl(fileName);
         return this.aLbumEntityUpdater.update(album);
     }
 }
