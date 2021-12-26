3c24bb2fc5d1f49dfba7781bc6d2ad77c97df960
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-26 20:28:32 +0200

    Some changes

---
+++ src/test/resources/subscribe-controller/deleteAllAfterTests.sql
@@ -0,0 +1,12 @@
+DELETE FROM user_roles WHERE TRUE;
+
+DELETE FROM artist_subscribers WHERE TRUE;
+DELETE FROM user_subscriptions WHERE TRUE;
+
+DELETE FROM artists WHERE TRUE;
+
+DELETE FROM subscriber WHERE TRUE;
+
+
+DELETE FROM users WHERE TRUE;
+
