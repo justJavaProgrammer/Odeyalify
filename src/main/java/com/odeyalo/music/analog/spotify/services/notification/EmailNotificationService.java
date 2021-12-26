0537b9ebe735d6faff9a254fcb5ad60edf9f0594
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-26 13:56:36 +0200

    Added notification method using MimeMessage

--- src/main/java/com/odeyalo/music/analog/spotify/services/notification/EmailNotificationService.java
+++ src/main/java/com/odeyalo/music/analog/spotify/services/notification/EmailNotificationService.java
@@ -1,13 +1,17 @@
 package com.odeyalo.music.analog.spotify.services.notification;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+
 @Service
 public class EmailNotificationService implements NotificationService {
-    private JavaMailSender mailSender;
-
+    private final JavaMailSender mailSender;
+    @Autowired
     public EmailNotificationService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
@@ -15,6 +19,12 @@ public class EmailNotificationService implements NotificationService {
     @Override
     @Async("taskExecutor")
     public void notifyUsers(SimpleMailMessage message) {
-            mailSender.send(message);
+        this.mailSender.send(message);
+    }
+
+    @Override
+    @Async("taskExecutor")
+    public void notifyUsers(MimeMessage mimeMessage) {
+        this.mailSender.send(mimeMessage);
     }
 }
