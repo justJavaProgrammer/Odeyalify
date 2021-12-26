b2abb9ef4da8877c8d7e9f80076600422fffbf01
Odeyalo <theprogrammerinfuture@gmail.com>
2021-12-26 14:01:39 +0200

    Some changes

--- src/main/java/com/odeyalo/music/analog/spotify/services/info/dao/SongInformation.java
+++ src/main/java/com/odeyalo/music/analog/spotify/services/info/dao/SongInformation.java
@@ -1,7 +1,7 @@
 package com.odeyalo.music.analog.spotify.services.info.dao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.odeyalo.music.analog.spotify.dto.SongDetailDTO;
+import com.odeyalo.music.analog.spotify.dto.detail.SongDetailDTO;
 
 public class SongInformation extends Information<SongDetailDTO> {
     private SongDetailDTO songDetailDTO;
