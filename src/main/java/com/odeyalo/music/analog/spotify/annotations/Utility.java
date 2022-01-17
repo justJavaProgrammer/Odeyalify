b7af809cef84dbf35366fcb4abecf58533263065
Odeyalo <theprogrammerinfuture@gmail.com>
2022-01-17 14:11:27 +0200

    Annotation for utility classes, use it instead Component annotation

---
+++ src/main/java/com/odeyalo/music/analog/spotify/annotations/Utility.java
@@ -0,0 +1,11 @@
+package com.odeyalo.music.analog.spotify.annotations;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface Utility {
+}
